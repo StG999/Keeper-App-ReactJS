05fef89115f988871984110f865192541c9133d2
StG999 <sidhant.yadav.ug21@nsut.ac.in>
2024-01-13 00:36:25 +0530

    add comment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Project by Sidhant Yadav. Made it while following Angela Yu's Course on ReactJS.
