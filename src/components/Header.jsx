c6174c78a559f0e884564f8db26e907d397fb287
StG999 <sidhant.yadav.ug21@nsut.ac.in>
2024-01-13 00:25:12 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MemoryIcon from '@mui/icons-material/Memory';
+
+function Header() {
+  return (
+    <header>
+      <h1><MemoryIcon fontSize="large" /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
